753fb3e24dccfbc12c2696d165d6f9a8be63ea53
gowthamvinod <gowthamvinod1220@gmail.com>
2025-10-02 12:57:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick =() => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text', {
+      strings: ['Python Developer','Graphic Designer','Web Designer'],
+      typeSpeed: 80,
+      backspeed: 80,
+      backDelay: 1200,
+      loop: true,
+    });
